27919b7441d0b3786ca8c95511060d7438602abc
Ariel <arielg.incgarcia@gmail.com>
2022-03-24 22:47:16 -0700

    Se agregaron mas imagenes, y una nueva vista al hacer clic en ellas

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './gallery.css';
+import CloseIcon from '@mui/icons-material/Close';
 
-import Img1 from './img/img1.png';
-import Img2 from './img/img2.png';
-import Img3 from './img/img3.png';
-import Img4 from './img/img4.png';
-import Img5 from './img/img5.png';
-import Img6 from './img/img6.png';
-import Img7 from './img/img7.png';
-import Img8 from './img/img8.png';
-import Img9 from './img/img9.png';
-import Img10 from './img/img10.png';
-import Img11 from './img/img11.png';
-import Img12 from './img/img12.png';
+import Img1 from './img/img1.jpg';
+import Img2 from './img/img2.jpg';
+import Img3 from './img/img3.jpg';
+import Img4 from './img/img4.jpg';
+import Img5 from './img/img5.jpg';
+import Img6 from './img/img6.jpg';
+import Img7 from './img/img7.jpg';
+import Img8 from './img/img8.jpg';
+import Img9 from './img/img9.jpg';
+import Img10 from './img/img10.jpg';
+import Img11 from './img/img11.jpg';
+import Img12 from './img/img12.jpg';
+import Img13 from './img/img13.jpg';
+import Img14 from './img/img14.jpg';
+import Img15 from './img/img15.jpg';
+import Img16 from './img/img16.jpg';
+import Img17 from './img/img17.jpg';
+import Img18 from './img/img18.jpg';
+import Img19 from './img/img19.jpg';
+import Img20 from './img/img20.jpg';
+import Img21 from './img/img21.jpg';
+import Img22 from './img/img22.jpg';
+import Img23 from './img/img23.jpg';
 
 const Gallery = () => 
 {
@@ -64,16 +76,71 @@ const Gallery = () =>
         {
             id: 12,
             imgSrc: Img12,
+        },
+        {
+            id: 13,
+            imgSrc: Img13,
+        },
+        {
+            id: 14,
+            imgSrc: Img14,
+        },
+        {
+            id: 15,
+            imgSrc: Img15,
+        },
+        {
+            id: 16,
+            imgSrc: Img16,
+        },
+        {
+            id: 17,
+            imgSrc: Img17,
+        },
+        {
+            id: 18,
+            imgSrc: Img18,
+        },
+        {
+            id: 19,
+            imgSrc: Img19,
+        },
+        {
+            id: 20,
+            imgSrc: Img20,
+        },
+        {
+            id: 21,
+            imgSrc: Img21,
+        },
+        {
+            id: 22,
+            imgSrc: Img22,
+        },
+        {
+            id: 23,
+            imgSrc: Img23,
         }
     ]
+    const [model, setModel] = useState(false);
+    const [tempimgSrc, setTempImgSrc]  = useState('');
+    const getImg = (imgSrc) =>
+    {
+        setTempImgSrc(imgSrc);
+        setModel(true);
+    }
 
     return(
         <>
+        <div className={model? "model open" : "model"}>
+            <img src={tempimgSrc}/>
+            <CloseIcon onClick={() => setModel(false)}/>
+        </div>
         <div className="gallery">
             {
                 data.map((item,index)=>{
                     return(
-                        <div className="pics" key={index}>
+                        <div className="pics" key={index} onClick={() => getImg(item.imgSrc)}>
                             <img src={item.imgSrc} style={{width: '100%'}}/>
                         </div>
                     )
